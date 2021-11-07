efb204b23ed367f09d3856a85836bda30053c1e6
genushomollama <gregorylitmannavarro@gmail.com>
2021-11-07 12:41:11 -0800

    Add a tester class

---
+++ longest-palindrome/LongestPalindromeTester.java
@@ -0,0 +1,13 @@
+public class LongestPalindromeTester {
+    public static void main(String[] args) {
+        String sampleIn = "hackerrekcahba";
+        String sampleOut = "hackerrekcah";
+        LongestPalindrome lp = LongestPalindrome();
+        String actualOut = lp.getLongestPalindrome(sampleIn);
+        if (actualOut.equals(sampleOut)) {
+            System.out.println("Success:", actualOut);
+        } else {
+            System.out.println("Failure:", actualOut);
+        }
+    }
+}
