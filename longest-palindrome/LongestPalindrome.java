5dc446185c2fc04a8e248cc31a743ae4577a9c81
genushomollama <gregorylitmannavarro@gmail.com>
2021-11-07 10:53:37 -0800

    Make an object, set a private field, call a function of that object.

--- longest-palindrome/LongestPalindrome.java
+++ longest-palindrome/LongestPalindrome.java
@@ -3,5 +3,18 @@
 class LongestPalindrome {
     public static void main(String[] args) {
         System.out.println("Hey baby!");
+        LongestPalindrome lp = new LongestPalindrome();
+        System.out.println(lp.toString());
+    }
+
+    // members can go here
+    private String instanceVar;
+
+    public LongestPalindrome() {
+        instanceVar = "blarghargh";
+    }
+
+    public String toString() {
+        return instanceVar;
     }
 }
